f3848b8e144aa773d44448df5c4f72521c350455
israel2x <ezurita@fiec.espol.edu.ec>
2024-04-30 20:18:22 -0500

    inicio de repositorio

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_clima_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_clima_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_clima_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
